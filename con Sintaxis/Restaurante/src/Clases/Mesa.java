3b170e156f606d021e1e276762bb8bd907a246ae
Rene Melendez <rene.melendeza@gmail.com>
2019-03-30 17:04:16 -0600

    Lista para empezar

--- con Sintaxis/Restaurante/src/Clases/Mesa.java
+++ con Sintaxis/Restaurante/src/Clases/Mesa.java
@@ -1,12 +1,13 @@
 package Clases;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Mesa {
     private int cantidad;
     private String nombre_mesa;
     private Double Total_mesa;
-
+    ArrayList<Cliente> clientes =new ArrayList<>();
     //crear array clientes, hasta que se de el desvergue de los metodos
 
     public Mesa(){
@@ -15,10 +16,10 @@ public class Mesa {
         this.Total_mesa=0.0;
     }
 
-    public Mesa(int cantidad, String nombre_mesa, Double total_mesa) {
+    public Mesa(int cantidad, String nombre_mesa) {
         this.cantidad = cantidad;
         this.nombre_mesa = nombre_mesa;
-        Total_mesa = total_mesa;
+        //Total_mesa = total_mesa;
     }
 
     public int getCantidad() {
@@ -43,5 +44,21 @@ public class Mesa {
 
     public void setTotal_mesa(Double total_mesa) {
         Total_mesa = total_mesa;
+    }
+
+    public void crear_cliente(){
+        for(int i=0; i<this.cantidad; i++){
+            Scanner dato = new Scanner(System.in);
+            String name=null;
+            int edad=0;
+            Cliente uncliente = new Cliente();
+            System.out.println("Nombre: ");
+            name=dato.nextLine();
+            System.out.println("Edad: ");
+            edad=dato.nextInt();dato.nextLine();
+            uncliente.setEdad(edad);
+            uncliente.setNombre(name);
+            clientes.add(uncliente);
+        }
     }
 }
--- con Sintaxis/Restaurante/src/Restaurante/Main.java
+++ con Sintaxis/Restaurante/src/Restaurante/Main.java
@@ -1,8 +1,45 @@
 package Restaurante;
 
+import Clases.Mesa;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int cantidad,op;
+        boolean continuar=true;
+        String nombre;
+        Scanner dato = new Scanner(System.in);
+        ArrayList<Mesa> mesas = new ArrayList<>();
+
+        while(continuar) {
+            System.out.println("Bienvenidos al Restaurate\n¿Qué desea hacer?\n\r\t 1.Crear nueva mesa\n\r\t2. Agregar un nuevo cliente a una mesa existente\n\r\t3.Pedir cuenta de una mesa");
+            op=dato.nextInt();dato.nextLine();
+            switch (op){
+                case 1:
+                    Mesa unamesa= new Mesa();
+                    System.out.println("Cantidad de personas: ");
+                    cantidad= dato.nextInt();dato.nextLine();
+                    System.out.println("Nombre de la mesa: ");
+                    nombre=dato.nextLine();
+                    unamesa.setCantidad(cantidad);
+                    unamesa.setNombre_mesa(nombre);
+                    unamesa.crear_cliente();
+                    mesas.add(unamesa);
+
+
+                    break;
+                case 2:
+                    break;
+
+
+
+
+            }
+        }
+
+
     }
 }
